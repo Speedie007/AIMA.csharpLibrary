b2f150e5ebec43db398f403afc285328b0fe4153
Brendan Wood <brendanw@mweb.co.za>
2024-05-11 00:14:53 +0200

    Agent Program Section feat: Added IPrecept Interface We use the percept to refer to the agent's perceptual inputs at any give instant. Date  10 May 2024 Done By: Brendan Wood

---
+++ AIMA.csharpLibaray/AgentProgram/Agent/Interface/IPrecept.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Runtime.Intrinsics.X86;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIMA.csharpLibrary.AgentProgram.Agent.Interface
+{
+    /// <summary>
+    /// <para>
+    /// Artificial Intelligence A Modern Approach (3rd Edition): pg 34.
+    /// </para>
+    /// We use the term percept to refer the agent's perceptual inputs at any give instant.
+    /// <para>
+    /// <remarks>
+    ///  This implementation serves as marker interface but the framework does not require to use it.
+    /// </remarks>
+    /// ///<para>
+    ///Author:Ravi Mohan
+    ///</para>
+    ///<para>
+    ///Author:Ciaran O'Reilly
+    ///</para>
+    ///<para>
+    ///Author:Ruediger Lunde
+    /// </para>
+    ///<para>
+    ///Author:Brendan Wood (Bsc. IT) - Complied C# Implementation - Supplemental
+    ///</para>
+    ///<para>Date Created: 10 May 2024 - Date Last Updated: 10 May 2024</para>
+    /// </para>
+    /// </summary>
+    public partial interface IPrecept
+    {
+    }
+}
