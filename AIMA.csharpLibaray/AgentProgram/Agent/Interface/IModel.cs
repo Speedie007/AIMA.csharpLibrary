b64febd100ba7d9a8e9012cc8b5499b91efee473
Brendan Wood <brendanw@mweb.co.za>
2024-05-11 00:29:52 +0200

    feat: Agent Program Section - Added IModel
    This knowledge about "how the world works" - whether implemented in simple. Boolean circuits or in complete scientific theories - is called a model of the world.An Agent that uses such a model is called a model-based agent.
    
    Date  10 May 2024
    Done By: Brendan Wood

---
+++ AIMA.csharpLibaray/AgentProgram/Agent/Interface/IModel.cs
@@ -0,0 +1,23 @@
+﻿namespace AIMA.csharpLibrary.AgentProgram.Agent.Interface
+{
+    /// <summary>
+    /// <para>Artificial Intelligence A Modern Approach (3rd Edition): pg 50.</para>
+    /// <para>
+    /// This knowledge about "how the world works" - whether implemented in simple
+    /// Boolean circuits or in complete scientific theories - is called a model of
+    /// the world.An Agent that uses such a model is called a model-based agent.
+    ///</para>
+    ///<para>
+    ///Author:Ciaran O'Reilly
+    ///</para>
+    ///<para>
+    ///Author:Brendan Wood (Bsc. IT) - Complied C# Implementation - Supplemental
+    ///</para>
+    ///<para>Date Created: 10 May 2024 - Date Last Updated: 10 May 2024</para>
+    /// </summary>
+    public partial interface IModel
+    {
+
+    }
+}
+ 
