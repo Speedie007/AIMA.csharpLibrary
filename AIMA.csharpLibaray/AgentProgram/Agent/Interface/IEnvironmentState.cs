18a8930f392c3e305e470728e11dd9b46cb73efa
Brendan Wood <brendanw@mweb.co.za>
2024-05-11 00:52:01 +0200

    feat: Agent Program Section - Added IEviromentState
    An interface used to indicate a possible state of an Environment.
    
    Date  11 May 2024
    Done By: Brendan Wood

---
+++ AIMA.csharpLibaray/AgentProgram/Agent/Interface/IEnvironmentState.cs
@@ -0,0 +1,18 @@
+﻿namespace AIMA.csharpLibrary.AgentProgram.Agent.Interface
+{
+    /// <summary>
+    /// <para>
+    /// An interface used to indicate a possible state of an Environment.
+    /// </para>
+    ///<para>
+    ///Author:Ciaran O'Reilly
+    ///</para>
+    ///<para>
+    ///Author:Brendan Wood (Bsc. IT) - Complied C# Implementation - Supplemental
+    ///</para>
+    ///<para>Date Created: 10 May 2024 - Date Last Updated: 10 May 2024</para>
+    /// </summary>
+    public partial interface IEnvironmentState
+    {
+    }
+}
