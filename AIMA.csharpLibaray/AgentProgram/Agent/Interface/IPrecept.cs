58a62e257808e9598aac2a1e877258965946be4f
Brendan Wood <brendanw@mweb.co.za>
2024-05-11 00:21:29 +0200

    chore: Agent Program Section Cleaning
    House Keeping - IPrecept Interface, removed unnessary using statements.
    
    Date  10 May 2024
    Done By: Brendan Wood

--- AIMA.csharpLibaray/AgentProgram/Agent/Interface/IPrecept.cs
+++ AIMA.csharpLibaray/AgentProgram/Agent/Interface/IPrecept.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection.Metadata;
-using System.Runtime.Intrinsics.X86;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace AIMA.csharpLibrary.AgentProgram.Agent.Interface
+﻿namespace AIMA.csharpLibrary.AgentProgram.Agent.Interface
 {
     /// <summary>
     /// <para>
