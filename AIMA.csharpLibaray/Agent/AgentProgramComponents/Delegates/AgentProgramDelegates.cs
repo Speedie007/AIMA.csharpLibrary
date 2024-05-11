b3339b7fc485a24a1176664b3fe2bdad6646fa2c
Brendan Wood <brendanw@mweb.co.za>
2024-05-11 11:46:30 +0200

    feat(agent program section): Added AgentProgramDelegates Class
    The DELEGATE => AgentProgramDelegates Class.
    The ApplyPrecept was added it handles the Concrete "Apply" methods in the base and concrete class that implement the IAgentProgam Interface.
    
    Date  11 May 2024
    Done By: Brendan Wood

---
+++ AIMA.csharpLibaray/Agent/AgentProgramComponents/Delegates/AgentProgramDelegates.cs
@@ -0,0 +1,28 @@
+﻿using AIMA.csharpLibrary.AgentProgram.Agent.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIMA.csharpLibrary.Agent.AgentProgramComponents.Delegates
+{
+    /// <summary>
+    /// Delegates Used To reference the Agent Program Methods/Functions
+    ///<para>
+    ///Author:Brendan Wood (Bsc. IT) - Complied C# Implementation - Supplemental
+    ///</para>
+    ///<para>Date Created: 10 May 2024 - Date Last Updated: 11 May 2024</para>
+    /// </summary>
+    /// <typeparam name="TPrecept">Type which is used to represent percepts</typeparam>
+    /// <typeparam name="TAction">Type which is used to represent actions</typeparam>
+    public class AgentProgramDelegates<TAction,TPrecept>
+    {
+        /// <summary>
+        /// Processes the relevant logic accosiated with the assigned Apply methods defined in the Concrete Agent Program Instantance.
+        /// </summary>
+        /// <param name="percept">Precept precieved by the agent program</param>
+        /// <returns></returns>
+        public delegate TAction? ApplyPrecept(TPrecept percept);
+    }
+}
