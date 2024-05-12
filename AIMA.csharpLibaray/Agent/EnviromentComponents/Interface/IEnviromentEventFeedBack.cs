598ec18ab275923098792bc1ce7bd176b47cc51f
Brendan Wood <brendanw@mweb.co.za>
2024-05-12 13:00:13 +0200

    feat(enviroment components): Completed the implementation of the event arguments.
    The IEnviromentEventFeedBack Interface defines the three main feed back methods whiich will be used in phase one. Namely OnAgentAdded, OnAgentRemoved, and OnAgentActed

--- AIMA.csharpLibaray/Agent/EnviromentComponents/Interface/IEnviromentEventFeedBack.cs
+++ AIMA.csharpLibaray/Agent/EnviromentComponents/Interface/IEnviromentEventFeedBack.cs
@@ -10,18 +10,45 @@ namespace AIMA.csharpLibrary.Agent.EnviromentComponents.Interface
     ///</para>
     ///<para>Date Created: 12 May 2024 - Date Last Updated: 12 May 2024</para>
     /// </summary>
-    /// <typeparam name="TPrecept">Type which is used to represent percepts</typeparam>
-    /// <typeparam name="TAction">ype which is used to represent actions</typeparam>
+    /// <typeparam name="TAgent">Base Type which is used to represent the Agent</typeparam>
+    /// <typeparam name="TPrecept">Base Type which is used to represent Percepts</typeparam>
+    /// <typeparam name="TAction">Base Type which is used to represent Actions</typeparam>
     public partial interface IEnviromentEventFeedBack<TAgent, TPrecept, TAction>
         where TPrecept : class, new()
         where TAction : class, new()
         where TAgent : BaseAgent<TPrecept, TAction>, new()
     {
-        void OnAgentAdded(EnviromentAgentAddedEventArgs<TAgent, TPrecept, TAction> e);
 
-        void OnAgentRemoved(EnviromentAgentRemovedEventArgs<TAgent, TPrecept, TAction> e);
-
-        void OnAgentActed(EnviromentAgentActedEventArgs<TAgent, TPrecept, TAction> e);
+        /// <summary>
+        /// Will raise the OnAgentAdded Event delegate which will multi cast to all subscribed listners the agent that was added to the enviroment.
+        /// </summary>
+        /// <remarks>
+        /// Note: The Event Delegate is in the BaseEnviroment Class
+        /// </remarks>
+        /// <param name="args">
+        /// The args parameter will retun the Agent that was added the Precept if applicable and the acction if applicable
+        /// </param>
+        void OnAgentAdded(EnviromentAgentAddedEventArgs<TAgent, TPrecept, TAction> args);
+        /// <summary>
+        /// Will raise the OnAgentRemoved Event delegate which will multi cast to all subscribed listners the agent that was removed from the enviroment.
+        /// </summary
+        /// <remarks>
+        /// Note: The Event Delegate is in the BaseEnviroment Class
+        /// </remarks>
+        /// <param name="args">
+        /// The args parameter will retun the Agent that was added the Precept if applicable and the acction if applicable
+        /// </param>
+        void OnAgentRemoved(EnviromentAgentRemovedEventArgs<TAgent, TPrecept, TAction> args);
+        /// <summary>
+        /// Will raise the OnAgentActed Event delegate which will multi cast to all subscribed listners the agent is executing and acting in the enviroment.
+        /// </summary
+        /// <remarks>
+        /// Note: The Event Delegate is in the BaseEnviroment Class
+        /// </remarks>
+        /// <param name="args">
+        /// The args parameter will retun the Agent which perform the action the Precept current observed and the Action which initiaed the action.
+        /// </param>
+        void OnAgentActed(EnviromentAgentActedEventArgs<TAgent, TPrecept, TAction> args);
 
     }
 }
