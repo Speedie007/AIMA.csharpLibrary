9840078e8e01673a7baa11ad31a01b3bf806ec20
Brendan Wood <brendanw@mweb.co.za>
2024-05-12 12:53:11 +0200

    feat(utilities): Custom data structure added: => LinkedHaskedSet.
    The internal datastruture of the LinkedHashSet comprises of a combination of a Dictonary and LinkedList this was used to balance the access effiecientcy of the dictonary and the sequency integrity of the dataset with the linkedlist.

--- AIMA.csharpLibaray/Utilities/LinkedHashSet.cs
+++ AIMA.csharpLibaray/Utilities/LinkedHashSet.cs
@@ -2,230 +2,295 @@ using System.Collections;
 
 namespace aima.core.util
 {
-    public class LinkedHashSet<T> : ISet<T> {
+    public class LinkedHashSet<T> : ISet<T>
+    where T : class
+    {
 
-    private readonly IDictionary<T, LinkedListNode<T>> dict;
-    private readonly LinkedList<T> list;
+        private readonly IDictionary<T, LinkedListNode<T>> dict;
+        private readonly LinkedList<T> list;
 
-    public LinkedHashSet(int initialCapacity) {
-        this.dict = new Dictionary<T,LinkedListNode<T>>(initialCapacity);
-        this.list = new LinkedList<T>();
-    }
-
-    public LinkedHashSet() {
-        this.dict = new Dictionary<T,LinkedListNode<T>>();
-        this.list = new LinkedList<T>();
-    }
-
-    public LinkedHashSet(IEnumerable<T> e) : this() {
-        addEnumerable(e);
-    }
-
-    public LinkedHashSet(int initialCapacity, IEnumerable<T> e) : this(initialCapacity) {
-        addEnumerable(e);
-    }
-
-    private void addEnumerable(IEnumerable<T> e) {
-        foreach (T t in e) {
-            Add(t);
+        public LinkedHashSet(int initialCapacity)
+        {
+            dict = new Dictionary<T, LinkedListNode<T>>(initialCapacity);
+            list = new LinkedList<T>();
         }
-    }
 
-    // ISet implementation
+        public LinkedHashSet()
+        {
+            dict = new Dictionary<T, LinkedListNode<T>>();
+            list = new LinkedList<T>();
+        }
 
-    public bool Add(T item) {
-        if (this.dict.ContainsKey(item)) {
-            return false;
+        public LinkedHashSet(IEnumerable<T> e) : this()
+        {
+            addEnumerable(e);
         }
-        LinkedListNode<T> node = this.list.AddLast(item);
-        this.dict[item] = node;
-        return true;
-    }
 
-    public void ExceptWith(IEnumerable<T> other) {
-        if (other == null) {
-            throw new ArgumentNullException("other cannot be null");
+        public LinkedHashSet(int initialCapacity, IEnumerable<T> e) : this(initialCapacity)
+        {
+            addEnumerable(e);
         }
-        foreach (T t in other) {
-            Remove(t);
-        }
-    }
 
-    public void IntersectWith(IEnumerable<T> other) {
-        if (other == null) {
-            throw new ArgumentNullException("other cannot be null");
+        private void addEnumerable(IEnumerable<T> e)
+        {
+            foreach (T t in e)
+            {
+                Add(t);
+            }
         }
-        T[] ts = new T[Count];
-        CopyTo(ts, 0);
-        foreach (T t in ts) {
-            if (!System.Linq.Enumerable.Contains(other, t)) {
+
+        // ISet implementation
+
+        public bool Add(T item)
+        {
+            if (dict.ContainsKey(item))
+            {
+                return false;
+            }
+            LinkedListNode<T> node = list.AddLast(item);
+            dict[item] = node;
+            return true;
+        }
+
+        public void ExceptWith(IEnumerable<T> other)
+        {
+            if (other == null)
+            {
+                throw new ArgumentNullException("other cannot be null");
+            }
+            foreach (T t in other)
+            {
                 Remove(t);
             }
         }
-    }
 
-    public bool IsProperSubsetOf(IEnumerable<T> other) {
-        if (other == null) {
-            throw new ArgumentNullException("other cannot be null");
-        }
-        int contains = 0;
-        int noContains = 0;
-        foreach (T t in other) {
-            if (Contains(t)) {
-                contains++;
-            } else {
-                noContains++;
+        public void IntersectWith(IEnumerable<T> other)
+        {
+            if (other == null)
+            {
+                throw new ArgumentNullException("other cannot be null");
+            }
+            T[] ts = new T[Count];
+            CopyTo(ts, 0);
+            foreach (T t in ts)
+            {
+                if (!Enumerable.Contains(other, t))
+                {
+                    Remove(t);
+                }
             }
         }
-        return contains == Count && noContains > 0;
-    }
 
-    public bool IsProperSupersetOf(IEnumerable<T> other) {
-        if (other == null) {
-            throw new ArgumentNullException("other cannot be null");
-        }
-        int otherCount = System.Linq.Enumerable.Count(other);
-        if (Count <= otherCount) {
-            return false;
-        }
-        int contains = 0;
-        int noContains = 0;
-        foreach (T t in this) {
-            if (System.Linq.Enumerable.Contains(other, t)) {
-                contains++;
-            } else {
-                noContains++;
+        public bool IsProperSubsetOf(IEnumerable<T> other)
+        {
+            if (other == null)
+            {
+                throw new ArgumentNullException("other cannot be null");
             }
+            int contains = 0;
+            int noContains = 0;
+            foreach (T t in other)
+            {
+                if (Contains(t))
+                {
+                    contains++;
+                }
+                else
+                {
+                    noContains++;
+                }
+            }
+            return contains == Count && noContains > 0;
         }
-        return contains == otherCount && noContains > 0;
-    }
 
-    public bool IsSubsetOf(IEnumerable<T> other) {
-        if (other == null) {
-            throw new ArgumentNullException("other cannot be null");
-        }
-        foreach (T t in this) {
-            if (!System.Linq.Enumerable.Contains(other, t)) {
+        public bool IsProperSupersetOf(IEnumerable<T> other)
+        {
+            if (other == null)
+            {
+                throw new ArgumentNullException("other cannot be null");
+            }
+            int otherCount = System.Linq.Enumerable.Count(other);
+            if (Count <= otherCount)
+            {
                 return false;
             }
+            int contains = 0;
+            int noContains = 0;
+            foreach (T t in this)
+            {
+                if (System.Linq.Enumerable.Contains(other, t))
+                {
+                    contains++;
+                }
+                else
+                {
+                    noContains++;
+                }
+            }
+            return contains == otherCount && noContains > 0;
         }
-        return true;
-    }
 
-    public bool IsSupersetOf(IEnumerable<T> other) {
-        if (other == null) {
-            throw new ArgumentNullException("other cannot be null");
+        public bool IsSubsetOf(IEnumerable<T> other)
+        {
+            if (other == null)
+            {
+                throw new ArgumentNullException("other cannot be null");
+            }
+            foreach (T t in this)
+            {
+                if (!System.Linq.Enumerable.Contains(other, t))
+                {
+                    return false;
+                }
+            }
+            return true;
         }
-        foreach (T t in other) {
-            if (!Contains(t)) {
+
+        public bool IsSupersetOf(IEnumerable<T> other)
+        {
+            if (other == null)
+            {
+                throw new ArgumentNullException("other cannot be null");
+            }
+            foreach (T t in other)
+            {
+                if (!Contains(t))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public bool Overlaps(IEnumerable<T> other)
+        {
+            if (other == null)
+            {
+                throw new ArgumentNullException("other cannot be null");
+            }
+            foreach (T t in other)
+            {
+                if (Contains(t))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public bool SetEquals(IEnumerable<T> other)
+        {
+            if (other == null)
+            {
+                throw new ArgumentNullException("other cannot be null");
+            }
+            int otherCount = System.Linq.Enumerable.Count(other);
+            if (Count != otherCount)
+            {
                 return false;
             }
+            return IsSupersetOf(other);
         }
-        return true;
-    }
 
-    public bool Overlaps(IEnumerable<T> other) {
-        if (other == null) {
-            throw new ArgumentNullException("other cannot be null");
-        }
-        foreach (T t in other) {
-            if (Contains(t)) {
-                return true;
+        public void SymmetricExceptWith(IEnumerable<T> other)
+        {
+            if (other == null)
+            {
+                throw new ArgumentNullException("other cannot be null");
+            }
+            T[] ts = new T[Count];
+            CopyTo(ts, 0);
+            HashSet<T> otherList = new HashSet<T>(other);
+            foreach (T t in ts)
+            {
+                if (otherList.Contains(t))
+                {
+                    Remove(t);
+                    otherList.Remove(t);
+                }
+            }
+            foreach (T t in otherList)
+            {
+                Add(t);
             }
         }
-        return false;
-    }
 
-    public bool SetEquals(IEnumerable<T> other) {
-        if (other == null) {
-            throw new ArgumentNullException("other cannot be null");
-        }
-        int otherCount = System.Linq.Enumerable.Count(other);
-        if (Count != otherCount) {
-            return false;
-        }
-        return IsSupersetOf(other);
-    }
-
-    public void SymmetricExceptWith(IEnumerable<T> other) {
-        if (other == null) {
-            throw new ArgumentNullException("other cannot be null");
-        }
-        T[] ts = new T[Count];
-        CopyTo(ts, 0);
-        HashSet<T> otherList = new HashSet<T>(other);
-        foreach (T t in ts) {
-            if (otherList.Contains(t)) {
-                Remove(t);
-                otherList.Remove(t);
+        public void UnionWith(IEnumerable<T> other)
+        {
+            if (other == null)
+            {
+                throw new ArgumentNullException("other cannot be null");
+            }
+            foreach (T t in other)
+            {
+                Add(t);
             }
         }
-        foreach (T t in otherList) {
-            Add(t);
+
+        // ICollection<T> implementation
+
+        public int Count
+        {
+            get
+            {
+                return dict.Count;
+            }
         }
-    }
 
-    public void UnionWith(IEnumerable<T> other) {
-        if (other == null) {
-            throw new ArgumentNullException("other cannot be null");
+        public bool IsReadOnly
+        {
+            get
+            {
+                return dict.IsReadOnly;
+            }
         }
-        foreach (T t in other) {
-            Add(t);
+
+        void ICollection<T>.Add(T item)
+        {
+            Add(item);
         }
-    }
 
-    // ICollection<T> implementation
-    
-    public int Count {
-        get {
-            return this.dict.Count;
+        public void Clear()
+        {
+            dict.Clear();
+            list.Clear();
         }
-    }
 
-    public bool IsReadOnly {
-        get {
-            return this.dict.IsReadOnly;
+        public bool Contains(T item)
+        {
+            return dict.ContainsKey(item);
         }
-    }
 
-    void ICollection<T>.Add(T item) {
-        Add(item);
-    }
-
-    public void Clear() {
-        this.dict.Clear();
-        this.list.Clear();
-    }
-
-    public bool Contains(T item) {
-        return this.dict.ContainsKey(item);
-    }
-
-    public void CopyTo(T[] array, int arrayIndex) {
-        this.list.CopyTo(array, arrayIndex);
-    }
-
-    public bool Remove(T item) {
-        LinkedListNode<T> node;
-        if (!this.dict.TryGetValue(item, out node)) {
-            return false;
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            list.CopyTo(array, arrayIndex);
         }
-        this.dict.Remove(item);
-        this.list.Remove(node);
-        return true;
-    }
 
-    // IEnumerable<T> implementation
-    
-    public IEnumerator<T> GetEnumerator() {
-        return this.list.GetEnumerator();
-    }
+        public bool Remove(T item)
+        {
+            LinkedListNode<T> node;
+            if (!dict.TryGetValue(item, out node))
+            {
+                return false;
+            }
+            dict.Remove(item);
+            list.Remove(node);
+            return true;
+        }
 
-    // IEnumerable implementation
-    
-    IEnumerator IEnumerable.GetEnumerator() {
-        return this.list.GetEnumerator();
-    }
+        // IEnumerable<T> implementation
 
-}
+        public IEnumerator<T> GetEnumerator()
+        {
+            return list.GetEnumerator();
+        }
+
+        // IEnumerable implementation
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return list.GetEnumerator();
+        }
+
+    }
 }
