7813c971424812ea2013c65087d8099117b9baab
Brendan Wood <brendanw@mweb.co.za>
2024-05-12 12:56:36 +0200

    init(testing: NUnit Project added to the libaray.
    The unit test libaray employed is NUnit.

---
+++ AIMA.csharp.Tests/DemoTest.cs
@@ -0,0 +1,16 @@
+namespace AIMA.csharp.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
