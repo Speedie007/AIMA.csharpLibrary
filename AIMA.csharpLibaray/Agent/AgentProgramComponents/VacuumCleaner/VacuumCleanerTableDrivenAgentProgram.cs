1803c3cb52ffb325d5ea51f7df44fb3e91cf893b
Brendan Wood <brendanw@mweb.co.za>
2024-05-16 15:10:28 +0200

    feat(vaccum cleaner examples): added the derived table driven agent program

---
+++ AIMA.csharpLibaray/Agent/AgentProgramComponents/VacuumCleaner/VacuumCleanerTableDrivenAgentProgram.cs
@@ -0,0 +1,22 @@
+﻿using AIMA.csharpLibrary.Agent.AgentComponents;
+using AIMA.csharpLibrary.Agent.AgentProgramComponents.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIMA.csharpLibrary.Agent.AgentProgramComponents.VacuumCleaner
+{
+    public partial class VacuumCleanerTableDrivenAgentProgram<TPrecept, TAction> : TableDrivenAgentProgram<TPrecept, TAction>
+        where TAction : AgentAction
+        where TPrecept : AgentPrecept
+    {
+        #region cstor
+        public VacuumCleanerTableDrivenAgentProgram(Dictionary<List<TPrecept>, TAction> table) : base(table)
+        {
+        }
+        #endregion
+
+    }
+}
