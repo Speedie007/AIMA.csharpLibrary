1d69d3f4392db302ce633711ba366f7fec0df81e
Brendan Wood <brendanw@mweb.co.za>
2024-05-17 07:48:38 +0200

    feat(search problem): implemented the IStepCostFunction interface.
    The step cost of taking action a in state s to reach state s' is denoted by c(s, a, s'). This is implemented by the ApplyAsDouble mehtod defined in the interface.

---
+++ AIMA.csharpLibaray/Search/Components/Interface/ISearchForActions.cs
@@ -0,0 +1,10 @@
+﻿namespace AIMA.csharpLibrary.Search.Components.Interface
+{
+    public partial interface ISearchForActions<TState,TAction>
+    {
+
+        List<TAction> findActions(Problem<TState, TAction> problem);
+
+        Metrics GetMetrics();
+    }
+}
