24b5b1d0ddaa95172f9aa5b72cb0710042ddd4e9
Brendan Wood <brendanw@mweb.co.za>
2024-05-17 07:14:21 +0200

    feat(agent components): implemented the int problem soliving agent

---
+++ AIMA.csharpLibaray/Agent/AgentComponents/Base/ProblemSolvingAgent.cs
@@ -0,0 +1,9 @@
+﻿namespace AIMA.csharpLibrary.Agent.AgentComponents.Base
+{
+    public partial  class ProblemSolvingAgent<TPrecept, TStates, TAction>: BaseAgent<TPrecept,TAction>
+        where TAction : AgentAction
+         where TPrecept : AgentPrecept
+    {
+
+    }
+}
