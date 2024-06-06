2fa68baf1efdb5d38fd5fe364bd793b3efee4c48
Brendan Wood <brendanw@mweb.co.za>
2024-06-06 20:40:33 +0200

    docs: Updated the comments

--- AIMA.CSharpLibaray/AgentImplementations/VacuumCleaner/Actions/BaseVacuumCleanerMovementAction.cs
+++ AIMA.CSharpLibaray/AgentImplementations/VacuumCleaner/Actions/BaseVacuumCleanerMovementAction.cs
@@ -4,6 +4,13 @@ using AIMA.CSharpLibrary.Common.DataStructure;
 
 namespace AIMA.CSharpLibrary.AgentImplementations.VacuumCleaner.Actions
 {
+    /// <summary>
+    /// <para>Base(Abstract) Enviroment used to represent the domain within wich the agent operates.</para>
+    ///<para>
+    ///Author:Brendan Wood (Bsc. IT) - Complied C# Implementation - Supplemental
+    ///</para>
+    ///<para>Date Created: 6 June 2024 - Date Last Updated: 6 June 2024</para>
+    /// </summary>
     public abstract partial class BaseVacuumCleanerMovementAction : BaseAgentAction, IVacuumCleanerMovementAction
     {
         public BaseVacuumCleanerMovementAction(string name) : base(name)
