5994cca6f51310e3e657d5f1d3433820ca10bcf5
Brendan Wood <brendanw@mweb.co.za>
2024-06-21 12:18:46 +0200

    feat: Updated project File

--- AIMA.csharpLibaray/AgentComponents/Agent/AgentActionTracker.cs
+++ AIMA.csharpLibaray/AgentComponents/Agent/AgentActionTracker.cs
@@ -15,9 +15,9 @@ namespace AIMA.CSharpLibrary.AgentComponents.Agent
     /// <typeparam name="TAction"></typeparam>
     public partial class AgentActionTracker<TAgent, TPrecept, TAction> :
         IEnviromentEventFeedBack<TAgent, TPrecept, TAction>
-            where TAction : BaseAction,new()
+            where TAction : AbstractAction,new()
             where TPrecept : BasePrecept, new()
-            where TAgent : BaseAgent<TPrecept, TAction>
+            where TAgent : AbstractAgent<TPrecept, TAction>
     {
         /// <summary>
         /// 
